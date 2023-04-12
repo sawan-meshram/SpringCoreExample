7530b7f258376a31bd686419d0aaa785e201d491
shatam-25 <shatam-25@shatam-25>
2023-04-12 14:21:21 +0530

    This is demo on spring core framework

---
+++ src/main/java/com/springcore/entities/Student.java
@@ -0,0 +1,40 @@
+package com.springcore.entities;
+
+public class Student {
+	private int id;
+	private String name;
+	private String address;
+	private Student() {
+		super();
+	}
+	private Student(int id, String name, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+	
+	
+}
